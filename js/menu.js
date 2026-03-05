617132c0823360b6180d34caa6b68616ef1880e7
husnaumeran <husna.umeran@gmail.com>
2026-03-05 05:52:34 -0600

    Update js/menu.js

--- js/menu.js
+++ js/menu.js
@@ -41,7 +41,7 @@ function showMenu() {
         {title:' Thinking', color:'#0099FF', items:[
             ['showColors','Color Patterns 🎨','Color Patterns'],['showColorsL2','Color Patterns L2 🎨','Color Patterns L2'],
             ['showDoesntBelong','Doesn\'t Belong 🤔','Doesn\'t Belong'],
-            ['showJora','Find Jora 🧩','Find Jora'],['showConnectDots','Connect Dots ✍️','Connect Dots'],
+            ['showJora','Find Pairs 🧩','Find Pairs'],['showConnectDots','Connect Dots ✍️','Connect Dots'],
             ['showFigureMatrices','Figure Matrices 🧩','Figure Matrices'],
             ['showVerbalAnalogies','Verbal Analogies 🗣️','Verbal Analogies']
         ]},
@@ -83,7 +83,7 @@ function startDaily() {
     const sections = [
         [['showAddition','Addition'],['showSubtraction','Subtraction'],['showCounting','Counting'],['showMatchNumbers','Match Numbers'],['showMoreLess','More/Less'],['showBiggerSmaller','Bigger/Smaller'],['showWhatNext','What Comes Next']],
         [['showTwoLetter','2-Letter Words'],['showThreeLetter','3-Letter Words'],['showTraceABC','Trace ABC'],['showTraceLower','Trace abc'],['showTraceNumbers','Trace Numbers']],
-        [['showColors','Color Patterns'],['showColorsL2','Color Patterns L2'],['showDoesntBelong','Doesn\'t Belong'],['showJora','Find Jora'],['showConnectDots','Connect Dots'],['showFigureMatrices','Figure Matrices'],['showVerbalAnalogies','Verbal Analogies']],
+        [['showColors','Color Patterns'],['showColorsL2','Color Patterns L2'],['showDoesntBelong','Doesn\'t Belong'],['showJora','Find Pairs'],['showConnectDots','Connect Dots'],['showFigureMatrices','Figure Matrices'],['showVerbalAnalogies','Verbal Analogies']],
         [['showUrduReading','Urdu Reading'],['showUrduTrace','Urdu Trace'],['showUrdu2Letter','Urdu 2-Letter Words'],['showUrduWhatNext','Urdu What Next']],
         [['showArabicQaida','Arabic Qaida']]
     ];
