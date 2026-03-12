e99280ea095a3c66297f9069be5a4bf88580c9ce
husnaumeran <husna.umeran@gmail.com>
2026-03-11 22:14:06 -0500

    Update js/worksheets/tracelower.js

--- js/worksheets/tracelower.js
+++ js/worksheets/tracelower.js
@@ -1,6 +1,6 @@
 // ============ TRACE LOWERCASE ============
 function showTraceLower() {
-    const letters = 'abcdefghijklmnopqrstuvwxyz'.slice(0, getFocusNumber('trace_lower')).split('');
+    const letters = 'abcdefghijklmnopqrstuvwxyz'.slice(0, getQuestionCount('trace_lower')).split('');
     let current = 0;
     const saved = {};
 
